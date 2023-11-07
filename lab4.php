69bd2f6e5f17e6e3350356b10b492ffc5898b2e0
Hola <56066640+xlmrf@users.noreply.github.com>
2023-11-07 23:18:31 +0200

    Create lab4.php

---
+++ lab4.php
@@ -0,0 +1,15 @@
+<?php
+
+//Don't echo anything, will take to much time
+function crack($hash){
+//G00D LUCK
+$counter = '00000';
+$md5 = md5($counter);
+
+while($counter != '99999' && $md5 != $hash){
+$counter = str_pad((int)$counter + 1, strlen($counter), '0', STR_PAD_LEFT);
+$md5 = md5($counter);
+}
+return $counter;
+
+}
