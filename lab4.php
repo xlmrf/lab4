43d8974d502b70a4094c90bc0ff19a15a845c8f2
Hola <56066640+xlmrf@users.noreply.github.com>
2023-11-07 23:19:41 +0200

    Update lab4.php

--- lab4.php
+++ lab4.php
@@ -7,7 +7,7 @@ $counter = '00000';
 $md5 = md5($counter);
 
 while($counter != '99999' && $md5 != $hash){
-$counter = str_pad((int)$counter + 1, strlen($counter), '0', STR_PAD_LEFT);
+$counter = str_pad(($counter + 1, strlen($counter), '0', STR_PAD_LEFT);
 $md5 = md5($counter);
 }
 return $counter;
